d11ecb18d76f864cfb7f01b935ea2cea4f720a39
stefanthr <83087927+stefanthr@users.noreply.github.com>
2022-11-01 23:03:59 +0100

    first test written

--- SalesTaxesProblem/src/test/ReceiptTest.java
+++ SalesTaxesProblem/src/test/ReceiptTest.java
@@ -1,5 +1,16 @@
 package test;
 
-public class ReceiptTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+import main.Receipt;
+
+public class ReceiptTest {
+	
+	@Test
+	public void getTotals() {
+		Receipt receipt = new Receipt();
+		assertEquals(0,receipt.calculateTotals(),0);
+	}
 }

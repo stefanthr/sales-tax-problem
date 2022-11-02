c2773308df6987821c87cd8024f575d684255bc5
stefanthr <83087927+stefanthr@users.noreply.github.com>
2022-11-02 10:28:43 +0100

    one step back on enum itemtype

--- SalesTaxesProblem/src/main/ItemType.java
+++ SalesTaxesProblem/src/main/ItemType.java
@@ -1,22 +1,12 @@
 package main;
 
 public enum ItemType {
-	BOOK(true, false), MEDICAL(true, false), FOOD(true, false), OTHERS(false, false), IMPORTED_BOOK(true, true),
-	IMPORTED_MEDICAL(true, true), IMPORTED_FOOD(true, true), IMPORTED_OTHERS(false, true);
-
-	private boolean isExempted;
-	private boolean isImported;
-
-	private ItemType(boolean exempted, boolean imported) {
-		isExempted = exempted;
-		isImported = imported;
-	}
-
-	public boolean isImported() {
-		return isImported;
-	}
-
-	public boolean isExempted() {
-		return isExempted;
-	}
+	BOOK, 
+	MEDICAL, 
+	FOOD, 
+	OTHERS, 
+	IMPORTED_BOOK,
+	IMPORTED_MEDICAL, 
+	IMPORTED_FOOD, 
+	IMPORTED_OTHERS;
 }
